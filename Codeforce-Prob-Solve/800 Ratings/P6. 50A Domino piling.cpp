4737729422fc15a260aef5e8911651427b4d8c26
Saikat Talukder <saikat60mu@gmail.com>
2025-09-14 20:15:01 +0600

    Add files via upload

---
+++ Codeforce-Prob-Solve/800 Ratings/P6. 50A Domino piling.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int M, N;
+    cin >> M >> N;
+    int total = M * N;
+    int result = total / 2;
+    cout << result;
+    return 0;
+}
