afb7e18556ce8e22ff9536ef7c19a778b78d7819
Saikat Talukder <saikat60mu@gmail.com>
2025-09-15 21:39:45 +0600

    Add files via upload

---
+++ Codeforce-Prob-Solve/800 Ratings/P8. 112A Petya and Strings.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a;
+    string b;
+    cin >> a >> b;
+    for (int i = 0; i < a.size(); i++)
+    {
+        a[i] = towlower(a[i]);
+        b[i] = towlower(b[i]);
+    }
+    if (a > b)
+    {
+        cout << "1";
+    }
+    else if (a < b)
+    {
+        cout << "-1";
+    }
+    else
+    {
+        cout << "0";
+    }
+    return 0;
+}
