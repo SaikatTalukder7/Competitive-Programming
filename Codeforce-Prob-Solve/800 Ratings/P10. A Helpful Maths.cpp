65ad59ffc7ec76045e7179e48dea71b139fe5adb
Saikat Talukder <saikat60mu@gmail.com>
2025-09-18 12:06:51 +0600

    Create P10. A Helpful Maths.cpp

---
+++ Codeforce-Prob-Solve/800 Ratings/P10. A Helpful Maths.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string s;
+    cin >> s;
+    vector<char> numbers;
+    for (char c : s)
+    {
+        if (c != '+')
+        {
+            numbers.push_back(c);
+        }
+    }
+    sort(numbers.begin(), numbers.end());
+    for (size_t i = 0; i < numbers.size(); i++)
+    {
+        cout << numbers[i];
+        if (i != numbers.size() - 1)
+        {
+            cout << "+";
+        }
+    }
+    return 0;
+}
