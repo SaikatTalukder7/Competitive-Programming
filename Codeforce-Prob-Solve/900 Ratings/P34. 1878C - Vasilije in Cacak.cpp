10b923bfd522dd6af162358ce20f48fc02baa611
Saikat Talukder <saikat60mu@gmail.com>
2025-10-24 19:41:02 +0600

    Create P34. 1878C - Vasilije in Cacak.cpp

---
+++ Codeforce-Prob-Solve/900 Ratings/P34. 1878C - Vasilije in Cacak.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        long long n, k, x;
+        cin >> n >> k >> x;
+
+        long long minSum = k * (k + 1) / 2;
+        long long maxSum = k * (2 * n - k + 1) / 2;
+
+        if (x >= minSum && x <= maxSum)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
