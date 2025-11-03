f31b5f6dea759c189f84d9ec632c3d0de64e7a7d
Saikat Talukder <saikat60mu@gmail.com>
2025-11-03 23:52:56 +0600

    Add files via upload

---
+++ Codeforce-Prob-Solve/1000 Ratings/P4. 479A - Expression.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int ans1 = a + b + c;
+    int ans2 = a * b * c;
+    int ans3 = (a + b) * c;
+    int ans4 = a * (b + c);
+
+    cout << max({ans1, ans2, ans3, ans4}) << endl;
+
+    return 0;
+}
