495cc8b3464b19f4a46faac59489f8f5e785bdb6
Saikat Talukder <saikat60mu@gmail.com>
2025-11-06 21:06:51 +0600

    Create P42. 25A - IQ test.cpp

---
+++ Codeforce-Prob-Solve/800 Ratings/P42. 25A - IQ test.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> a[i];
+    }
+
+    vector<int> even, odd;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] % 2 == 0) 
+        {
+            even.push_back(i + 1);
+        } 
+        else 
+        {
+            odd.push_back(i + 1);
+        }
+    }
+
+    if (even.size() == 1) 
+    {
+        cout << even[0] <<endl;
+    } 
+    else 
+    {
+        cout << odd[0] << endl;
+    }
+
+    return 0;
+}
