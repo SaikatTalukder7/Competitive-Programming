40be5d7d1a9519b038dc54210e3249f952da1c33
Saikat Talukder <saikat60mu@gmail.com>
2025-11-07 18:45:51 +0600

    Create P43. 268A - Games.cpp

---
+++ Codeforce-Prob-Solve/800 Ratings/P43. 268A - Games.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n), a(n);
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> arr[i];
+        cin >> a[i];
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            if (i != j && arr[i] == a[j]) 
+            {
+                count++;
+            }
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
