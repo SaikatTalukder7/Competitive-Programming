f74834f988bd6be62796f92b7f64da0955718687
Saikat Talukder <saikat60mu@gmail.com>
2025-11-25 21:47:21 +0600

    Create P47. 1972B - Coin Games.cpp

---
+++ Codeforce-Prob-Solve/P47. 1972B - Coin Games.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int cntU = 0;
+        for (char c : s)
+        {
+            if (c == 'U') cntU++;
+        }
+        if (cntU % 2 == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
