ff30bd60d1dc8bfa2113c1960716dc080948053c
Saikat Talukder <saikat60mu@gmail.com>
2026-01-07 20:42:53 +0600

    Create A - Binary Array Game.cpp

---
+++ Contest/Hello 2026/A - Binary Array Game.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void saikat() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int x;
+        for (int i = 0; i < n; i++) {
+            cin >> x;
+        }
+        {
+            cout << "Alice" << endl;
+        }
+    }
+}
+
+int main() {
+    {
+        saikat();
+    }
+    return 0;
+}
