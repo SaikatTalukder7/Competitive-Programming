d3d9b0f1bcac49b034d2afaad0cb5d4565e50fd4
Saikat Talukder <saikat60mu@gmail.com>
2026-01-28 21:03:48 +0600

    Create 1352C - K-th Not Divisible by n.cpp

---
+++ Extra/1352C - K-th Not Divisible by n.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, k;
+        cin >> n >> k;
+        long long ans = k + (k - 1) / (n - 1);
+        cout << ans << endl;
+    }
+    return 0;
+}
