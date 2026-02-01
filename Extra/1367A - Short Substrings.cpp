608831e66f6e4a430f690e947388611b87e5a9e5
Saikat Talukder <saikat60mu@gmail.com>
2026-02-01 22:37:41 +0600

    Create 1367A - Short Substrings.cpp

---
+++ Extra/1367A - Short Substrings.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string b;
+        cin >> b;
+        string a;
+        a.push_back(b[0]);
+        for (int i = 1; i < (int)b.size(); i += 2)
+        {
+            a.push_back(b[i]);
+        }
+        cout << a << endl;
+    }
+    return 0;
+}
