18823e4219657a6be1b7b14f506e18669a1d9ef7
Saikat Talukder <saikat60mu@gmail.com>
2026-02-17 22:06:32 +0600

    Create 500A - New Year Transportation.cpp

---
+++ Extra/500A - New Year Transportation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, t;
+    cin >> n >> t;
+
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n - 1; i++) {
+        cin >> a[i];
+    }
+
+    int pos = 1;
+
+    while (pos < t) {
+        pos = pos + a[pos];
+    }
+
+    if (pos == t)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
