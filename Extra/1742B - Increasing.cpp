5ac68bbaf6f0093b0e5bf396abfc7abfa923e1cd
Saikat Talukder <saikat60mu@gmail.com>
2026-02-20 21:39:53 +0600

    Create 1742B - Increasing.cpp

---
+++ Extra/1742B - Increasing.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+
+    while (t--) 
+    {
+        int n;
+        cin >> n;
+
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        sort(a.begin(), a.end());
+
+        bool ok = true;
+        for (int i = 1; i < n; i++) {
+            if (a[i] == a[i - 1]) {
+                ok = false;
+                break;
+            }
+        }
+
+        if (ok) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+    return 0;
+}
