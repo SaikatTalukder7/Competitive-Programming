841a2d99ddbc93ecb1f9464f8080c0d956e827fc
Saikat Talukder <saikat60mu@gmail.com>
2026-03-04 22:59:44 +0600

    Update 1985B - Maximum Multiple Sum.cpp

--- Extra/1985B - Maximum Multiple Sum.cpp
+++ Extra/1985B - Maximum Multiple Sum.cpp
@@ -1,15 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
+void solve() {
+    long long n;
+    cin >> n;
+
+    long long bestX = 2;
+    long long maxSum = 0;
+
+    for (long long x = 2; x <= n; x++) {
+        long long currentSum = 0;
+
+        for (long long multiple = x; multiple <= n; multiple += x) {
+            currentSum += multiple;
+        }
+
+        if (currentSum > maxSum) {
+            maxSum = currentSum;
+            bestX = x;
+        }
+    }
+
+    cout << bestX << endl;
+}
 
 int main() {
     int t;
     cin >> t;
 
-    while(t--) {
-        int n;
-        cin >> n;
-
-        cout << 2 << endl;
+    while (t--) {
+        solve();
     }
 
     return 0;
