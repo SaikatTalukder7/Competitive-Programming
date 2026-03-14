d247f7be4f08fe740bfe5f9a9ee2c2dffc38fc2a
Saikat Talukder <saikat60mu@gmail.com>
2026-03-14 20:13:29 +0600

    Create 707A - Brain's Photos.cpp

---
+++ Extra/707A - Brain's Photos.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    bool color = false;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            char c;
+            cin >> c;
+
+            if(c == 'C' || c == 'M' || c == 'Y')
+            {
+                color = true;
+            }
+        }
+    }
+
+    if(color)
+    {
+        cout << "#Color" << endl;
+    }
+    else
+    {
+        cout << "#Black&White" << endl;
+    }
+}
