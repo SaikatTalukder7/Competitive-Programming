88a210b4fad46e37ab9588509e09560dc76e7a01
Saikat Talukder <saikat60mu@gmail.com>
2026-04-13 22:07:34 +0600

    Create 1927A - Make it White.cpp

---
+++ Extra/1927A - Make it White.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int l = -1, r = -1;
+    for(int i = 0; i < n; i++) {
+        if(s[i] == 'B') {
+            if(l == -1) l = i;
+            r = i;
+        }
+    }
+    cout << r - l + 1 << "\n";
+}
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+}
