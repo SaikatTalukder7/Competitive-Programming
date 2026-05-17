8f74fd256800efbda2db772388d2ea8e7b11dad8
Saikat Talukder <saikat60mu@gmail.com>
2026-05-17 22:21:08 +0600

    Create 1374C - Move Brackets.cpp

---
+++ Extra/1374C - Move Brackets.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int bal = 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '(')
+        {
+            bal++;
+        }
+        else
+        {
+            bal--;
+        }
+        if (bal < 0)
+        {
+            ans++;
+            bal = 0;
+        }
+    }
+    cout << ans << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
