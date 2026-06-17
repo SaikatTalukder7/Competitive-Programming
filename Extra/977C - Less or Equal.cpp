715beea1d8f1079c410d6b775d8d40b25b324b1d
Saikat Talukder <saikat60mu@gmail.com>
2026-06-17 07:08:43 +0600

    Create 977C - Less or Equal.cpp

---
+++ Extra/977C - Less or Equal.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    if (k == 0)
+    {
+        if (a[0] == 1)
+        {
+            cout << -1 << "\n";
+        }
+        else
+        {
+            cout << a[0] - 1 << "\n";
+        }
+    }
+    else
+    {
+        int x = a[k - 1];
+        if (k < n && a[k] == x)
+        {
+            cout << -1 << "\n";
+        }
+        else
+        {
+            cout << x << "\n";
+        }
+    }
+    return 0;
+}
