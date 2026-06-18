f9675c4890d0eae2d3b9e961cc286a7c361b1034
Saikat Talukder <saikat60mu@gmail.com>
2026-06-18 23:53:08 +0600

    Create 2074A - Draw a Square.cpp

---
+++ Extra/2074A - Draw a Square.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int l, r, d, u;
+        cin >> l >> r >> d >> u;
+
+        if (l == r && r == d && d == u)
+        {
+            cout << "Yes\n";
+        }
+        else
+        {
+            cout << "No\n";
+        }
+    }
+
+    return 0;
+}
