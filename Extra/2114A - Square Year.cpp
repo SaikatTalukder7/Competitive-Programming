7bea605e52e082a046e61893a887907d01e58f87
Saikat Talukder <saikat60mu@gmail.com>
2026-07-12 19:28:08 +0600

    Create 2114A - Square Year.cpp

---
+++ Extra/2114A - Square Year.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        int num = stoi(s);
+
+        int root = sqrt(num);
+
+        if (root * root == num)
+        {
+            cout << 0 << " " << root << endl;
+        }
+        else
+        {
+            cout << -1 << endl;
+        }
+    }
+
+    return 0;
+}
