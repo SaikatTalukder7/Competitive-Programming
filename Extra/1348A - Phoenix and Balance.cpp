3a15535a8840acb5a7cb6144848ecc0c8c89ae1c
Saikat Talukder <saikat60mu@gmail.com>
2026-07-16 23:35:26 +0600

    Create 1348A - Phoenix and Balance.cpp

---
+++ Extra/1348A - Phoenix and Balance.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        long long pile1 = 0, pile2 = 0;
+        pile1 += (1LL << n);
+        for (int i = 1; i < n / 2; i++)
+        {
+            pile1 += (1LL << i);
+        }
+
+        for (int i = n / 2; i < n; i++)
+        {
+            pile2 += (1LL << i);
+        }
+
+        cout << abs(pile1 - pile2) << endl;
+    }
+
+    return 0;
+}
