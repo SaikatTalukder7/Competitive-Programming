8c15bd32186f45b6e4c5a908270b375519fab245
Saikat Talukder <saikat60mu@gmail.com>
2026-07-17 22:17:50 +0600

    Create 1385A - Three Pairwise Maximums.cpp

---
+++ Extra/1385A - Three Pairwise Maximums.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        vector<long long> v(3);
+
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> v[i];
+        }
+
+        sort(v.begin(), v.end());
+
+        if (v[1] != v[2])
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+            cout << v[0] << " " << v[2] << " " << v[0] << endl;
+        }
+    }
+
+    return 0;
+}
