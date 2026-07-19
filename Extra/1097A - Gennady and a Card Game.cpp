f7275819151ee02f108b23a2f4dd429753322599
Saikat Talukder <saikat60mu@gmail.com>
2026-07-19 22:48:37 +0600

    Create 1097A - Gennady and a Card Game.cpp

---
+++ Extra/1097A - Gennady and a Card Game.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string table;
+    cin >> table;
+    bool ok = false;
+    for (int i = 0; i < 5; i++)
+    {
+        string card;
+        cin >> card;
+        if (card[0] == table[0] || card[1] == table[1])
+        {
+            ok = true;
+        }
+    }
+    if (ok)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
