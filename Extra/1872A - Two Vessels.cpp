2e18caa923b69161c2630c00aeb3f066f2e1ddf2
Saikat Talukder <saikat60mu@gmail.com>
2026-07-21 23:49:34 +0600

    Create 1872A - Two Vessels.cpp

---
+++ Extra/1872A - Two Vessels.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        int diff = abs(a - b);
+
+        cout << (diff + 2 * c - 1) / (2 * c) << endl;
+    }
+
+    return 0;
+}
