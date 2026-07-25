edaa0bb2a12c353fa73ef4176502685de70c0047
Saikat Talukder <saikat60mu@gmail.com>
2026-07-25 23:50:48 +0600

    Create 1607A - Linear Keyboard.cpp

---
+++ Extra/1607A - Linear Keyboard.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string keyboard, s;
+        cin >> keyboard >> s;
+
+        vector<int> pos(26);
+
+        for (int i = 0; i < 26; i++)
+        {
+            pos[keyboard[i] - 'a'] = i;
+        }
+
+        int ans = 0;
+
+        for (int i = 1; i < s.size(); i++)
+        {
+            ans += abs(pos[s[i] - 'a'] - pos[s[i - 1] - 'a']);
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
