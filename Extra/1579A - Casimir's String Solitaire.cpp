de0a45749ab2c59976bdf7452adb1333272e142d
Saikat Talukder <saikat60mu@gmail.com>
2026-08-06 19:46:13 +0600

    Create 1579A - Casimir's String Solitaire.cpp

---
+++ Extra/1579A - Casimir's String Solitaire.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        int a = 0, b = 0, c = 0;
+
+        for (char ch : s)
+        {
+            if (ch == 'A')
+            {
+                a++;
+            }
+            else if (ch == 'B')
+            {
+                b++;
+            }
+            else
+            {
+                c++;
+            }
+        }
+
+        if (b == a + c)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
