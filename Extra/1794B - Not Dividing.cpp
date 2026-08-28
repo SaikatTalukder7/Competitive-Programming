365c50e5a63419d442aa0505b98fae52a89c36ac
Saikat Talukder <saikat60mu@gmail.com>
2026-08-28 07:51:56 +0600

    Create 1794B - Not Dividing.cpp

---
+++ Extra/1794B - Not Dividing.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define nline '\n'
+using namespace std;
+
+void Saikat()
+{
+    ll n;
+    cin >> n;
+
+    vector<ll> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+
+        if (arr[i] == 1)
+            arr[i]++;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i + 1] % arr[i] == 0)
+            arr[i + 1]++;
+    }
+
+    for (auto itr : arr)
+        cout << itr << " ";
+
+    cout << nline;
+}
+
+int main()
+{
+    ll T;
+    cin >> T;
+
+    while (T--)
+    {
+        Saikat();
+    }
+
+    return 0;
+}
